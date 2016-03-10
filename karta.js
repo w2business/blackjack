dfb10446eb08c0aa54f3d058e911740adff8d053
Andrii Stepura <Andrii Stepura>
2016-03-10 12:41:04 +0100

    Random card function.

---
+++ karta.js
@@ -0,0 +1,31 @@
+<meta charset="utf-8">
+<script>
+var karty=[6,7,8,9,10,"V","Q","K","A"];
+
+//Функция случайной карты
+function tianemKartu(){
+var karta=(Math.round(Math.random()*(karty.length-1)));
+return karta=karty[karta];
+}
+//Считаем карты игрока
+function pods4et(hand){
+var sum=0;
+for(var i=0;i<hand.length;i++){
+if(hand[i]=="V"||hand[i]=="Q"||hand[i]=="K"){
+hand[i]=10;}
+else if(hand[i]=="A"){
+if(sum<=10){
+hand[i]=11;}
+else {hand[i]=1;}
+}
+sum+=hand[i];}
+return sum;
+}
+
+//Раздаем карты и открываем их
+var diler=[tianemKartu()];
+var player=[tianemKartu(),tianemKartu()];
+alert("Карта дилера: " + diler + "\nКарты игрока: " + player.join(" и "));
+
+alert("Сумма карт игрока " + pods4et(player));
+</script>
